8ec469f240f664eea4f02b9badd7b90a4fe5c876
pradeep-poolstack232365 <pradeep.kushwaha@poolstack.in>
2024-02-13 00:02:24 +0530

    stripe_payment

--- Gemfile
+++ Gemfile
@@ -19,8 +19,9 @@ gem "jwt"
 
 gem "actionmailer"
 
+# stripe payment
 gem "stripe"
-
+gem 'dotenv-rails', :groups => [:development, :test]
 
 # UI interface debugging-with-the-debug-gem
 gem "sassc"
@@ -50,7 +51,7 @@ gem "redis", ">= 4.0.1"
 # gem "kredis"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_12_162450) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_12_180608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,6 +115,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_162450) do
     t.index ["product_id", "order_id"], name: "index_orders_products_on_product_id_and_order_id"
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.string "payment_intent_id"
+    t.string "payment_status"
+    t.string "checkout_session_id"
+    t.bigint "user_id"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -135,6 +145,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_12_162450) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.boolean "reset_password_used"
+    t.string "stripe_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
